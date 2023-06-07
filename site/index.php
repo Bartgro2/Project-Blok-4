1bb33caff192eb115332de302b821d9016b29a09
Saabbass <178414@novacollege.nl>
2023-06-07 09:17:11 +0200

    pagina's aanmaken

---
+++ site/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    include('header.php');
+    ?>
+    <section id="content">
+
+    </section>
+    <?php
+    include('footer.php');
+    ?>
+</body>
+
+</html>
