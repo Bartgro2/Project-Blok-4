ae0d0318db3d70ea52ad3e6353f71be1912beb6f
Bart Groothuysen <bart@groothuysen.com>
2023-06-07 08:57:44 +0200

    database connectie check gedaan

--- site/database.php
+++ site/database.php
@@ -7,3 +7,13 @@ $dbname = "projectblok4";
 
 // Maak een  database connectie
 $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+// Controleer de verbinding
+if(mysqli_connect_error())
+{
+ echo "Connection establishing failed!";
+}
+else
+{
+ echo "Connection established successfully.";
+}
