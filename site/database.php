7c230ef054839f4f73b6afe941441725afb4f612
Bart Groothuysen <bart@groothuysen.com>
2023-06-07 08:56:50 +0200

    database configuratie fix

--- site/database.php
+++ site/database.php
@@ -6,11 +6,4 @@ $dbpass = "password";
 $dbname = "projectblok4";
 
 // Maak een  database connectie
-$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);// Database configuratie
-$host  = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "GFG";
-
-// Maak een  database connectie
 $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
