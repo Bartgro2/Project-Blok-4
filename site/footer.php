60e73f51216988329717fbdfbb8299a90cc35c55
Saabbass <178414@novacollege.nl>
2023-06-08 09:28:05 +0200

    footer maken

---
+++ site/footer.php
@@ -0,0 +1,25 @@
+<div class="footer_container">
+    <div class="container">
+        <div class="container_width">
+            <!-- <div id="google_translate_element"></div>
+
+            <script type="text/javascript">
+                function googleTranslateElementInit() {
+                    new google.translate.TranslateElement({
+                        pageLanguage: 'en'
+                    }, 'google_translate_element');
+                }
+            </script>
+
+            <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script> -->
+            <div class="footer">
+                <h3>The footer</h3>
+                <h3>The footer</h3>
+                <h3>The footer</h3>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
