67d69b1e610464e05b453faa3b860dfecd2d511a
Saabbass <178414@novacollege.nl>
2023-06-08 09:28:33 +0200

    nav maken

---
+++ site/nav.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li>
+            <a href="index.php">Home</a>
+            <a href="">Services</a>
+            <a href="">About us</a>
+        </li>
+    </ul>
+    <a class="btn_contact" href="login.php">login</a>
+    <a class="btn_contact" href="">Contact</a>
+</nav>
