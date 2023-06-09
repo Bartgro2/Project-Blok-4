3bcd405305d00d4518dcfeb04215047fd68816af
Bart Groothuysen <bart@groothuysen.com>
2023-06-09 18:25:51 +0200

    nav.php toegevoegd

---
+++ site/nav.php
@@ -0,0 +1,12 @@
+<nav>
+    <div class="container">
+        <ul>
+            <li> <a href="index.php">Home</a></li>
+            <li> <a href="#">Recepten</a></li>
+            <li> <a href="#">Specials</a></li>
+        </ul>
+    </div>
+</nav>
+</body>
+
+</html>
