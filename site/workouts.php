8ff667aac43ebbfdbcfddd201812b3bfdb40fb51
Bart Groothuysen <bart@groothuysen.com>
2023-06-12 13:21:16 +0200

    workouts.php tabel structuur

---
+++ site/workouts.php
@@ -0,0 +1,46 @@
+<?php
+require 'database.php';
+
+$sql = "SELECT * FROM workout";
+
+$result = mysqli_query($conn, $sql);
+
+$workouts_info = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <main>
+        <table>
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>omschrijving</th>
+                    <th>duur</th>
+                    <th>notitie</th>
+                    <th>startdatum</th>
+                </tr>
+            </thead>
+            <?php foreach ($workouts_info as $workout_info) : ?>
+                <tbody>
+                    <td><?php echo $workout_info['id'] ?></td>
+                    <td><?php echo $workout_info['omschrijving'] ?></td>
+                    <td><?php echo $workout_info['duur'] ?></td>
+                    <td><?php echo $workout_info['notitie'] ?></td>
+                    <td><?php echo $workout_info['startdatum'] ?></td>
+                </tbody>
+            <?php endforeach; ?>
+        </table>
+    </main>
+    </div>
+</body>
+
+</html>
