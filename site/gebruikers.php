0386cdcb36fe1c29bab11cd7c0640266495db33b
Bart Groothuysen <bart@groothuysen.com>
2023-06-12 09:06:28 +0200

    gebruikers.php table aangemaakt

--- site/gebruikers.php
+++ site/gebruikers.php
@@ -1,11 +1,44 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+    <main>
+
+        <form action="verwerk-inloggen.php" method="post">
+
+            <div class="button-container">
+                <button type="submit" class="input-button"> Aanmelden</button>
+            </div>
+            <table>
+                <thead>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                </thead>
+
+
+
+
+                <tbody>
+                    </tr>
+                    <tr>
+                    <tr></tr>
+                    <tr></tr>
+                    <tr></tr>
+                </tbody>
+            </table>
+    </main>
 </body>
+
 </html>
