43eea0e11f5f9fac7d08ae00293db6ddcb1002cc
Bart Groothuysen <bart@groothuysen.com>
2023-06-12 13:49:11 +0200

    laatste nav check en dashboard basic code

---
+++ site/manager-dashboard.php
@@ -0,0 +1,26 @@
+<?php require 'database.php';
+session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <?php include 'nav.php'; ?>
+
+    <?php echo "Hallo! " . $_SESSION['username']; ?>
+
+    
+</body>
+
+</html>
--- site/nav.php
+++ site/nav.php
@@ -1,35 +1,32 @@
+
 <nav>
     <div class="container">
         <ul>
 
 
-            <?php if (empty($_SESSION['userid'])) { ?>
-                <li> <a href="index.php">Home</a></li>
-            <?php } ?>
-
-            <?php if (isset($_SESSION['userid'])) { ?>
-                <?php if ($_SESSION['userid'] == $_SESSION['adminid']) { ?>
+          
+            
+                <?php if (isset($_SESSION['adminid'])) { ?>
                     <li> <a href="index.php">Home</a></li>
                     <li> <a href="nieuwe-workout.php">adressen</a></li>
                     <li> <a href="workouts.php">adressen bekijken</a></li>
                     <li> <a href="gebruikers.php">gebruikers</a></li>          
                     <li> <a href="#">statistieken</a></li>
                     <li> <a href="logout.php">outloggen</a></li>
-                <?php } else if ($_SESSION['userid'] == $_SESSION['managerid']) { ?>
+                <?php } else if (isset($_SESSION['managerid'])) { ?>
                     <li> <a href="index.php">Home</a></li>
                     <li> <a href="nieuwe-workout.php">workout-a</a></li>
                     <li> <a href="workouts.php">workout</a></li>
                     <li> <a href="#">statistieken</a></li>
                     <li> <a href="logout.php">outloggen</a></li>
-                <?php } else if ($_SESSION['userid'] == $_SESSION['regularid']) { ?>
+                <?php } else if (isset($_SESSION['regularid'])) { ?>
                     <li> <a href="index.php">Home</a></li>
                     <li> <a href="workouts.php">workouts</a></li>
                     <li> <a href="logout.php">outloggen</a></li>
-            <?php } 
-            } ?>
-
-
-
+            <?php } else{?>
+                <li> <a href="index.php">Home</a></li>
+                <li> <a href="inloggen.php">inloggen</a></li>
+            <?php    } ?>
         </ul>
     </div>
 </nav>
