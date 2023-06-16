1dcfaaec77ab09b4169c949cb42a021a52a9501f
Bart Groothuysen <bart@groothuysen.com>
2023-06-16 09:36:28 +0200

    sql queries manager, regular , admin = workout

--- site/workouts.php
+++ site/workouts.php
@@ -1,11 +1,37 @@
 <?php
 require 'database.php';
 
-$sql = "SELECT * FROM workout";
+$sql = "SELECT *  FROM workout";
 
 $result = mysqli_query($conn, $sql);
 
 $workouts_info = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+$sql = "SELECT COUNT(id) as workouts From workout";
+
+$result = mysqli_query($conn, $sql);
+
+$aantal_workouts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+$sql = "SELECT COUNT(gebruiker.workout_id) as admins FROM workout INNER JOIN gebruiker ON gebruiker.workout_id = workout.id INNER JOIN administrator on administrator.gebruiker_id = gebruiker.id";
+
+$result = mysqli_query($conn, $sql);
+
+$aantal_administrators = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+$sql = "SELECT COUNT(gebruiker.workout_id) as managers FROM workout INNER JOIN gebruiker ON gebruiker.workout_id = workout.id INNER JOIN manager on manager.gebruiker_id = gebruiker.id";
+
+$result = mysqli_query($conn, $sql);
+
+$aantal_managers = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+$sql = "SELECT COUNT(gebruiker.workout_id) as regulars FROM workout INNER JOIN gebruiker ON gebruiker.workout_id = workout.id INNER JOIN regular on regular.gebruiker_id = gebruiker.id";
+
+$result = mysqli_query($conn, $sql);
+
+$aantal_regulars = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -21,30 +47,54 @@ $workouts_info = mysqli_fetch_all($result, MYSQLI_ASSOC);
     <?php require 'nav.php'; ?>
     <main>
         <div class="container-table">
-        <table>
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>omschrijving</th>
-                    <th>duur</th>
-                    <th>notitie</th>
-                    <th>startdatum</th>
-                </tr>
-            </thead>
-            <?php foreach ($workouts_info as $workout_info) : ?>
+            <table>
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>omschrijving</th>
+                        <th>duur</th>
+                        <th>notitie</th>
+                        <th>startdatum</th>
+                    </tr>
+                </thead>
+                <?php foreach ($workouts_info as $workout_info) : ?>
+                    <tbody>
+                        <td><?php echo $workout_info['id'] ?></td>
+                        <td><?php echo $workout_info['omschrijving'] ?></td>
+                        <td><?php echo $workout_info['duur'] ?></td>
+                        <td><?php echo $workout_info['notitie'] ?></td>
+                        <td><?php echo $workout_info['startdatum'] ?></td>
+                    </tbody>
+                <?php endforeach; ?>
+            </table>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>workouts</th>
+                        <th>admins</th>
+                        <th>managers</th>
+                        <th>regulars</th>
+                    </tr>
+                </thead>
                 <tbody>
-                    <td><?php echo $workout_info['id'] ?></td>
-                    <td><?php echo $workout_info['omschrijving'] ?></td>
-                    <td><?php echo $workout_info['duur'] ?></td>
-                    <td><?php echo $workout_info['notitie'] ?></td>
-                    <td><?php echo $workout_info['startdatum'] ?></td>
+                    <?php foreach ($aantal_workouts as $workout) : ?>
+                        <td><?php echo $workout['workouts'] ?></td>
+                    <?php endforeach; ?>
+                    <?php foreach ($aantal_administrators as $administrator) : ?>
+                        <td><?php echo $administrator['admins'] ?> </td>
+                    <?php endforeach; ?>
+                    <?php foreach ($aantal_managers as $manager) : ?>
+                        <td><?php echo $manager['managers'] ?></td>
+                    <?php endforeach; ?>
+                    <?php foreach($aantal_regulars as $regulars): ?>
+                    <td><?php echo $regulars['regulars'] ?></td>
+                    <?php endforeach; ?>
                 </tbody>
-            <?php endforeach; ?>
-        </table>
+            </table>
     </main>
     </div>
     <?php require 'footer.php'; ?>
 </body>
 
 </html>
-
