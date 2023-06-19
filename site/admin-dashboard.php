8833da5c7bbcbeb38637baf8279591391f830e39
Bart Groothuysen <bart@groothuysen.com>
2023-06-19 20:28:17 +0200

    dashboard styling

--- site/admin-dashboard.php
+++ site/admin-dashboard.php
@@ -15,7 +15,6 @@ $admin_gegevens = mysqli_fetch_assoc($result);
 
 
 
-
 ?>
 
 <!DOCTYPE html>
@@ -36,26 +35,33 @@ $admin_gegevens = mysqli_fetch_assoc($result);
 
         <p id="greeting" class="groet"> </p>
         <p class="groet"><?php echo $_SESSION['username']; ?></p>
-
+        <p><?php echo "in dienst sinds: " . $admin_gegevens['in dienst']; ?></p>
 
         <div class="admin-details">
-            
 
 
-            <p><?php echo $admin_gegevens['voornaam'] ?></p>
-            <p><?php echo $admin_gegevens['tussenvoegsels'] ?></p>
-            <p><?php echo $admin_gegevens['achternaam'] ?> </p>
-            <p><?php echo $admin_gegevens['geslacht'] ?></p>
-            <p><?php echo $admin_gegevens['email'] ?></p>
-            <p><?php echo $admin_gegevens['wachtwoord'] ?></p>
-            <p><?php echo $admin_gegevens['straat'] ?></p>
-            <p><?php echo $admin_gegevens['huisnummer'] ?></p>
-            <p><?php echo $admin_gegevens['postcode'] ?></p>
-            <p><?php echo $admin_gegevens['plaats'] ?></p>
-            <p><?php echo $admin_gegevens['land'] ?></p>
-            <p><?php echo $admin_gegevens['telefoonnummer'] ?></p>
-            <p><?php echo $admin_gegevens['mobielnummer'] ?></p>
+
+            <h3>Persoonlijke gegevens</h3>
+            <p><?php echo "Voornaam: " . $admin_gegevens['voornaam']; ?></p>
+            <p><?php echo "Tussenvoegsels: " . $admin_gegevens['tussenvoegsels']; ?></p>
+            <p><?php echo "Achternaam: " . $admin_gegevens['achternaam']; ?></p>
+            <p><?php echo "Geslacht: " . $admin_gegevens['geslacht']; ?></p>
+            <p><?php echo "Email: " . $admin_gegevens['email']; ?></p>
+            <p><?php echo "Wachtwoord: " . $admin_gegevens['wachtwoord']; ?></p>
+
+            <h3>Adresgegevens</h3>
+            <p><?php echo "Straat: " . $admin_gegevens['straat']; ?></p>
+            <p><?php echo "Huisnummer: " . $admin_gegevens['huisnummer']; ?></p>
+            <p><?php echo "Postcode: " . $admin_gegevens['postcode']; ?></p>
+            <p><?php echo "Plaats: " . $admin_gegevens['plaats']; ?></p>
+            <h3>Contactgegevens</h3>
+            <p><?php echo "Telefoonnummer: " . $admin_gegevens['telefoonnummer']; ?></p>
+            <p><?php echo "Mobielnummer: " . $admin_gegevens['mobielnummer']; ?></p>
+
+
+
         </div>
+    </div>
     </div>
     <script src="Javascript/script.js"> </script>
 </body>
